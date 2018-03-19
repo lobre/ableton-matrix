2246e8e76204dc89e572771aa1347217c9839e5a
Loric <loric.brevet@gmail.com>
2018-03-19 21:15:57 +0100

    Implement scenes and fire

--- matrix.js
+++ matrix.js
@@ -50,8 +50,19 @@ function removeFromArray(arr) {
 
 var Live = new Object();
 
-Live.liveSet = new LiveAPI("live_set");
-Live.liveSetView = new LiveAPI("live_set view");
+Live.LiveSet = function() {
+    if (!Live.liveSet) {
+        Live.liveSet = new LiveAPI("live_set");
+    }
+    return Live.liveSet;
+};
+
+Live.LiveSetView = function() {
+    if (!Live.liveSetView) {
+        Live.liveSetView = new LiveAPI("live_set view");
+    }
+    return Live.liveSetView;
+};
 
 Live.Log = function (liveObject) {
     log("         PATH:", liveObject.path);
@@ -69,17 +80,19 @@ Live.Parse = function (data, unique) {
 
 Live.Env = function () {
     return {
-        masterTrack: Live.Parse(Live.liveSet.get("master_track"), 1),
-        returnTracks: Live.Parse(Live.liveSet.get("return_tracks")),
-        tracks: Live.Parse(Live.liveSet.get("tracks")),
-        visibleTracks: Live.Parse(Live.liveSet.get("visible_tracks")),
-        selectedTrack: Live.Parse(Live.liveSetView.get("selected_track"), 1)
-    }
+        masterTrack: Live.Parse(Live.LiveSet().get("master_track"), 1),
+        returnTracks: Live.Parse(Live.LiveSet().get("return_tracks")),
+        tracks: Live.Parse(Live.LiveSet().get("tracks")),
+        visibleTracks: Live.Parse(Live.LiveSet().get("visible_tracks")),
+        selectedTrack: Live.Parse(Live.LiveSetView().get("selected_track"), 1),
+        scenes: Live.Parse(Live.LiveSet().get("scenes")),
+        selectedScene: Live.Parse(Live.LiveSetView().get("selected_scene"), 1)
+    };
 };
 
 Live.GetAllTrackIds = function () {
     return Live.Env().tracks.concat(Live.Env().returnTracks, Live.Env().masterTrack);
-}
+};
 
 Live.GetSelectedTrackIndex = function () {
     if (Live.Env().selectedTrack) {
@@ -90,15 +103,15 @@ Live.GetSelectedTrackIndex = function () {
         }
     }
     return -1;
-}
+};
 
 Live.GetVisibleTrackCount = function () {
     return Live.Env().visibleTracks.length;
-}
+};
 
 Live.SelectMasterTrack = function () {
-    Live.liveSetView.set("selected_track", "id", Live.Env().masterTrack);
-}
+    Live.LiveSetView().set("selected_track", "id", Live.Env().masterTrack);
+};
 
 Live.SelectNextTrack = function () {
     var visibleTrackCount = Live.GetVisibleTrackCount();
@@ -115,7 +128,7 @@ Live.SelectNextTrack = function () {
     } else {
         Live.SelectMasterTrack();
     }
-}
+};
 
 Live.SelectPreviousTrack = function () {
     var visibleTrackCount = Live.GetVisibleTrackCount();
@@ -132,14 +145,80 @@ Live.SelectPreviousTrack = function () {
     } else {
         Live.SelectMasterTrack();
     }
-}
+};
 
 Live.SetSelectedTrackIndex = function (index) {
     var visibleTrackCount = Live.GetVisibleTrackCount();
     if (index >= 0 && index < visibleTrackCount) {
-        Live.liveSetView.set("selected_track", "id", Live.Env().visibleTracks[index]);
+        Live.LiveSetView().set("selected_track", "id", Live.Env().visibleTracks[index]);
     }
-}
+};
+
+Live.FireSelectedScene = function() {
+    var selectedSceneIndex = Live.GetSelectedSceneIndex();
+    var selectedScene = new LiveAPI("live_set scenes " + selectedSceneIndex);
+    selectedScene.call("fire_as_selected");
+};
+
+Live.GetSceneCount = function() {
+    return Live.Env().scenes.length;
+};
+
+Live.GetSelectedSceneIndex = function() {
+    if (Live.Env().selectedScene) {
+        for (var i = 0; i < Live.Env().scenes.length; i++) {
+            if (Live.Env().scenes[i] == Live.Env().selectedScene) {
+                return i;
+            }
+        }
+    }
+    return -1;
+};
+
+Live.SelectNextScene = function() {
+    var sceneCount = Live.GetSceneCount();
+    var selectedSceneIndex = Live.GetSelectedSceneIndex();
+
+    if (sceneCount > 0) {
+        if (selectedSceneIndex >= 0 && selectedSceneIndex < sceneCount - 1) {
+            Live.SetSelectedSceneIndex(selectedSceneIndex + 1);
+        } else {
+            Live.SetSelectedSceneIndex(0);
+        }
+    }
+};
+
+Live.SelectPreviousScene = function() {
+    var sceneCount = Live.GetSceneCount();
+    var selectedSceneIndex = Live.GetSelectedSceneIndex();
+
+    if (sceneCount > 0) {
+        if (selectedSceneIndex > 0 && selectedSceneIndex < sceneCount) {
+            Live.SetSelectedSceneIndex(selectedSceneIndex - 1);
+        } else {
+            Live.SetSelectedSceneIndex(sceneCount - 1);
+        }
+    }
+};
+
+Live.SetSelectedSceneIndex = function(index) {
+    var sceneCount = Live.GetSceneCount();
+    if (index >= 0 && index < sceneCount) {
+        Live.LiveSetView().set("selected_scene", "id", Live.Env().scenes[index]);
+    }
+};
+
+Live.FireSelectedClip = function() {
+    var selectedSceneIndex = Live.GetSelectedSceneIndex();
+    var selectedTrackIndex = Live.GetSelectedTrackIndex();
+    if (selectedTrackIndex < 0) {
+        Live.FireSelectedScene();
+    } else {
+        var path = "live_set visible_tracks " + selectedTrackIndex + " clip_slots " + selectedSceneIndex;
+        var clip = new LiveAPI(path);
+        clip.call("fire");
+    }    
+};
 
 
 //--------------------------------------------------------------------
@@ -153,4 +232,16 @@ function nextTrack() {
 
 function previousTrack() {
     Live.SelectPreviousTrack();
+}
+
+function nextScene() {
+    Live.SelectNextScene();
+}
+
+function previousScene() {
+    Live.SelectPreviousScene();
+}
+
+function fire() {
+    Live.FireSelectedClip();
 }
